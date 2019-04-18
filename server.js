046e994a8d66a9429d438d06729af0474b96fc38
lequidong <lequidong@gmail.com>
2019-04-18 17:13:22 +0700

    EX

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+const EC = require('elliptic').ec;
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/src/public/wallet.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+router.get('/public_key', function(req,res) {
+    // Use secp256k1
+    const ec = new EC('secp256k1');
+
+    // Generate a new key pair and convert them to hex-strings
+    const key = ec.genKeyPair();
+    const publicKey = key.getPublic('hex');
+    const privateKey = key.getPrivate('hex');
+    
+    res.send([publicKey, privateKey]);
+});
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
